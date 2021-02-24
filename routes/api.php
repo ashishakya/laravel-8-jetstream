089b661a156e5240d44f7e7167b05fcb5e9bc79b
Ashish Shakya <ashish.shakya@yipl.com.np>
2021-02-24 23:02:25 +0545

    user Api token roundabout

--- routes/api.php
+++ routes/api.php
@@ -17,3 +17,10 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::middleware('auth:sanctum')->get('/test', function (Request $request) {
+    dd($request->user()->tokenCan('read'));
+    return response("hey") ;
+});
+
+
